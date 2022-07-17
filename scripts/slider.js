242433779dc846d00a1b9a294d7c33d4ab15c409
Bahhe <marchelldteach@gmail.com>
2022-07-17 03:15:19 +0100

    added multiple sections

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,5 +1,21 @@
-const slider = document.querySelectorAll('.product-showcase')
-setInterval(() => {
-  slider[0].classList.toggle('active')
-  slider[1].classList.toggle('active')
-}, 7000);
+// SECTION-ONE SLIDER 
+
+const itemsSlider = document.querySelector('.items-slider')
+const items = document.querySelector('.item-container')
+const leftBtn = document.querySelector('.left-slide-btn')
+const rightBtn = document.querySelector('.right-slide-btn')
+
+const itemWidth = items.getBoundingClientRect().width
+
+leftBtn.addEventListener('click', () => {
+  itemsSlider.scrollBy({
+    left: `-${itemWidth + 10}`,
+    behavior: 'smooth'
+  })
+})
+rightBtn.addEventListener('click', () => {
+  itemsSlider.scrollBy({
+    left: `${itemWidth + 10}`,
+    behavior: 'smooth'
+  })
+})
